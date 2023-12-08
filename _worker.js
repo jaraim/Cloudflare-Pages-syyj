7063c120f13788fccebd25dc3d0925b923b41ed6
jaraim <117310975+jaraim@users.noreply.github.com>
2023-12-08 12:19:32 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,11 @@ async fetch(request, env) {
   let url = new URL(request.url);
   if (url.pathname.match(/^\/|\/api\/*/)) {
     url.hostname = 'ph.jaraim.top'
-    let new_request = new Request(url, request);
+    let new_request = new Request(url, request, {
+      headers: {
+        'X-Forwarded-Host': url.hostname
+      }
+    });
     return fetch(new_request);
   }
 }

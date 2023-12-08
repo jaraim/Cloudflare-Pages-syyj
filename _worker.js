6fbb11a3a3335301dd231e016a65c775ec40cea3
jaraim <117310975+jaraim@users.noreply.github.com>
2023-12-08 12:14:33 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,11 +1,8 @@
-export default {
-  async fetch(request, env) {
-    let url = new URL(request.url);
-    if (url.pathname.startsWith('/')) {
-      url.hostname = 'ph.jaraim.top'
-      let new_request = new Request(url, request);
-      return fetch(new_request);
-    }
-    return env.ASSETS.fetch(request);
-  },
-};
+async fetch(request, env) {
+  let url = new URL(request.url);
+  if (url.pathname.match(/^\/|\/api\/*/)) {
+    url.hostname = 'ph.jaraim.top'
+    let new_request = new Request(url, request);
+    return fetch(new_request);
+  }
+}

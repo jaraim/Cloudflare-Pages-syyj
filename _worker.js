7d70b09d866a60c0beb7e8c72a0a36af3ecaa92e
jaraim <117310975+jaraim@users.noreply.github.com>
2024-02-07 00:44:13 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'ph.wupg.eu.org'
+      url.hostname = 'ph.cloudns.be'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
